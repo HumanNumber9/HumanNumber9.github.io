4e35348b6b790b8f139573939e24a29701ae739c
HumanNumber9 <86557490+HumanNumber9@users.noreply.github.com>
2021-09-01 17:49:59 +0700

    Create contract_ABI.js

---
+++ src/contract_ABI.js
@@ -0,0 +1,15 @@
+var contractABI = [
+	{
+		"inputs": [],
+		"name": "getWord",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
